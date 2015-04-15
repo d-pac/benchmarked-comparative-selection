278ab16238d8367db7ffc1cab1e67c4827537e17
Camille Reynders <camille.reynders@gmail.com>
2015-04-15 10:38:18 +0200

    Add initial test

---
+++ tests/select.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require( 'must' );
+var subject = require( '../lib/select' );
+
+describe( "-- select --", function(){
+  describe( "test", function(){
+    it( "should run", function(){
+      expect( false ).to.be.true();
+    } );
+  } );
+} );
