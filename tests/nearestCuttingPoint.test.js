6ab783b7bb9a04cb6f73cb665123f4921ecd8650
SanVerhavert <san.verhavert@uantwerpen.be>
2015-04-15 11:10:18 +0200

    nearestCuttingPoint added

---
+++ tests/nearestCuttingPoint.test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by SVerhavert on 24/03/2015.
+ */
+
+var CP = require('../lib/nearestCuttingPoint');
+
+var myBenchmarks = [ { _id:"AAAAAAAAA", ability:1.236}, { _id:"BBBBBBBBB", ability:-.65 } ];
+
+console.log(CP(1,myBenchmarks));
