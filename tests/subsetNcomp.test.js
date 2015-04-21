b4a990ddce782bf7a6d560d90bbb84b70af86412
SanVerhavert <san.verhavert@uantwerpen.be>
2015-04-21 16:20:30 +0200

    updated subsetting function and test

--- tests/subsetNcomp.test.js
+++ tests/subsetNcomp.test.js
@@ -3,9 +3,35 @@ var subsetNComp = require('../lib/subsetNComp');
 
 var Representations = [
   { _id: "reprA",
-    comparedNum: 3,
+    comparedNum: 1,
     closeTo:null
+  },
+  { _id: "reprB",
+    comparedNum: 2,
+    closeTo: "reprE"
+  },
+  { _id: "reprC",
+    comparedNum: 2,
+    closeTo: null
+  },
+  { _id: "reprD",
+    comparedNum: 1,
+    closeTo: "reprE"
+  },
+  { _id: "reprE",
+    comparedNum: 1,
+    closeTo: null
+  },
+  { _id: "reprF",
+    comparedNum: 5,
+    closeTo: "reprC"
+  },
+  { _id: "reprG",
+    comparedNum: 1,
+    closeTo: "reprE"
   }
 ];
 
-subsetNComp(Representations, "reprC");
+var candidateOpponents = subsetNComp(Representations, "reprE");
+
+console.log(candidateOpponents);
