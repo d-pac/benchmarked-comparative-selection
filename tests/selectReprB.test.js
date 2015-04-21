756b163a9cbc310b43c8305f2b5dd47f27603033
SanVerhavert <san.verhavert@uantwerpen.be>
2015-04-21 17:31:16 +0200

    added selectReprB and test. No working properly yet

---
+++ tests/selectReprB.test.js
@@ -0,0 +1,51 @@
+
+var selectReprB = require('../lib/selectReprB');
+
+var CandidateOppos = [
+  { _id: "reprA",
+    ability:{
+      value:0.123,
+      se: .1
+    }
+  },
+  { _id: "reprB",
+    ability: {
+      value: 6,
+      se: .1
+    }
+  },
+  { _id: "reprC",
+    ability: {
+      value: -6,
+      se: .1
+    }
+  },
+  { _id: "reprD",
+    ability:{
+      value:-0.123,
+      se: .1
+    }
+  },
+  { _id: "reprE",
+    ability:{
+      value:5.232,
+      se: .1
+    }
+  },
+  { _id: "reprF",
+    ability:{
+      value:5,
+      se: .1
+    }
+  },
+  { _id: "reprG",
+    ability:{
+      value:9,
+      se: .1
+    }
+  }
+];
+
+var reprB = selectReprB(CandidateOppos, 0);
+
+console.log(reprB);
