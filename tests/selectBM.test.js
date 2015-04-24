a15d22910ee0cca127837a68bf74ff0a285649b9
lcoertje <liesje.coertjens@uantwerpen.be>
2015-04-24 09:44:45 +0200

    phase 1, selectie representatie B

--- tests/selectBM.test.js
+++ tests/selectBM.test.js
@@ -3,39 +3,6 @@
  */
 var selectBM= require('../lib/selectBM.js');
 
-/*
- * Simple comparative selection algorithm
- * @param {Object[]} items - Array of items with following minimal structure:
- * @param {string} items[]._id - Unique identifier for the item
- * @param {string[]} items[].compared - Array of unique identifiers
- */
-/*function select( items ){
- *if( !items || !_.isArray( items ) || 2 > items.length ){
- *throw new Error( "comparative-selection#select: `items` should be an Array with a minimum length of 2" );
- *} else {
- *items = _.sortBy( _.shuffle( items ), function( item ){
- *return item.compared.length;
- *} );
- *var selected = items.shift();
- *var opponent;
- *if( 0 >= selected.compared.length ){
- *opponent = items.shift();
- *} else {
- *opponent = _.find( items, function( item ){
- *return 0 > item.compared.indexOf( selected._id );
- *} );
- *if( !opponent ){
- *opponent = items.shift();
- *}
- *}
- *return [ selected, opponent ];
- *}
- *}
- *exports = module.exports = {
- *select : select
- };*/
-
-
 
 
 var representation = [{
